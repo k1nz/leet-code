1248eb2742c2a6ec517ccb4de5d6211ac2fd50ba
k1nz <kinz.qiu@foxmail.com>
2022-03-04 00:02:47 +0800

    feat: 每日一体

---
+++ 每日一题/22-03/258.各位相加.ts
@@ -0,0 +1,13 @@
+// 递归
+function addDigitsRecursion(num: number): number {
+  const arr = num.toString().split('')
+  if (arr.length === 1) return num
+  return addDigitsRecursion(
+    arr.reduce((prev, curr) => prev + parseInt(curr), 0)
+  )
+}
+
+// 数学
+function addDigitsMath(num: number): number {
+  return ((num - 1) % 9) + 1
+}
